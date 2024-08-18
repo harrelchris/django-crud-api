aa975b47e9a9cc686a4ef0273c42e10aa0848e44
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-08-18 00:40:03 -0500

    Configure admin

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
+admin.site.index_title = "Site administration"
+admin.site.name = "Django Admin"
+admin.site.site_header = "Django administration"
+admin.site.site_title = "Django site admin"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
