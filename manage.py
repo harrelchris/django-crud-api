66859105f1f703b512dcdae0135269c1ea1389c4
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-08-18 00:19:50 -0500

    Relocate manage.py to top level directory

--- manage.py
+++ manage.py
@@ -2,12 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 
 import os
+import pathlib
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.core.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,6 +17,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    current_path = pathlib.Path(__file__).parent.resolve()
+    sys.path.append(str(current_path / "app"))
     execute_from_command_line(sys.argv)
 
 
