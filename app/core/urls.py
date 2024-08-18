7d626150a36e8333567460f32aa1589fa3f1af61
Chris Harrel <harrelchris@gmail.com>
2024-08-18 16:24:25 -0500

    Route schema and drf-spectacular endpoints

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 admin.site.index_title = "Site administration"
 admin.site.name = "Django Admin"
@@ -8,6 +9,10 @@ admin.site.site_title = "Django site admin"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls"), name="rest_framework"),
+    path("api/auth/", include("rest_framework.urls"), name="rest_framework"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    # Routers
     path("", include("items.urls"), name="items"),
 ]
